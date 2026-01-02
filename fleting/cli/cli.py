7846d541539cd8f5ac7d26c9160fd3b5ff5a32a8
Alex Yucra <xalextrack@gmail.com>
2026-01-02 15:58:09 -0300

    cli update 1.0.4

--- fleting/cli/cli.py
+++ fleting/cli/cli.py
@@ -1,7 +1,8 @@
 import sys
-
+from fleting.cli.commands.init import handle_init
 from fleting.cli.commands.create import handle_create
 from fleting.cli.commands.delete import handle_delete
+
 
 def main():
     try:
@@ -10,10 +11,22 @@ def main():
         if not args:
             print("Uso: fleting create <controller|view|model|page> <nome>")
             return
+        
+        if not args or args[0] in ("-h", "--help"):
+            print("""
+Fleting CLI
+
+Uso:
+  fleting init                  # init new project
+  fleting create page <nome>    # create new page
+""")
+            return
 
         command = args[0]
 
-        if command == "create":
+        if command == "init"
+            handle_init()
+        elif command == "create":
             handle_create(args[1:])
         elif command == "delete":
             handle_delete(args[1:])
---
+++ fleting/cli/commands/init.py
@@ -0,0 +1,4 @@
+from fleting.setup import init_project
+
+def handle_init():
+    init_project()
